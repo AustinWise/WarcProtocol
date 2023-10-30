b6039335ad1abfe39a36b3082e0d696325849dbf
Austin Wise <AustinWise@gmail.com>
2023-10-29 17:40:07 -0700

    Add benchmark

---
+++ tests/Toimik.WarcProtocol.Benchmarks/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Toimik.WarcProtocol.Benchmarks;
+
+using BenchmarkDotNet.Running;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        BenchmarkRunner.Run(typeof(Program).Assembly, config: null, args);
+    }
+}
---
+++ tests/Toimik.WarcProtocol.Benchmarks/WarcParserBenchmark.cs
@@ -0,0 +1,29 @@
+﻿namespace Toimik.WarcProtocol.Benchmarks;
+
+using BenchmarkDotNet.Attributes;
+
+[MemoryDiagnoser]
+public class WarcParserBenchmark
+{
+    readonly byte[] warcFile;
+
+    public WarcParserBenchmark()
+    {
+        using var resourceStream = typeof(WarcParserBenchmark).Assembly.GetManifestResourceStream("Toimik.WarcProtocol.Benchmarks.response.warc");
+        using var memoryStream = new MemoryStream();
+        resourceStream!.CopyTo(memoryStream);
+        warcFile = memoryStream.ToArray();
+    }
+
+    [Benchmark]
+    public async Task<int> ReadFile()
+    {
+        var parser = new WarcParser();
+        int recordCount = 0;
+        await foreach (var record in parser.Parse(new MemoryStream(warcFile, writable: false), isCompressed: false))
+        {
+            recordCount += 1;
+        }
+        return recordCount;
+    }
+}
